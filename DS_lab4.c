8ddc9b9a082cf01fbcee4af74e82c9062bf09cc3
shipra1212 <95070458+shipra1212@users.noreply.github.com>
2022-03-24 10:26:09 +0530

    Add files via upload

---
+++ DS_lab4.c
@@ -0,0 +1,7 @@
+/*Construct a structure of a “Mobile” object with following attributes/features
+1)     Mobile :model—String
+2)     Brand: Name—String
+3)     Memory: Capacity—Integer
+4)     Price—Float*/
+
+
