331f8071cf53c39a6ce91f85ad059b9e9ce07a8e
shipra1212 <95070458+shipra1212@users.noreply.github.com>
2022-04-21 09:21:56 +0530

    Add files via upload

---
+++ DS_lab8_part2.c
@@ -0,0 +1,95 @@
+/*
+2147155 shipra jha
+ PATTERN MATCHING USING KMP METHOD
+*/
+
+
+#include<stdio.h>
+#include<string.h>
+ 
+void computeLPSArray(char* pat, int M, int* lps);
+ 
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char* pat, char* txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+ 
+    // create lps[] for holding  the longest prefix suffix for given pattern.
+    int lps[M];
+ 
+    computeLPSArray(pat, M, lps);
+ 
+    int i = 0; // index for txting
+    int j = 0;
+    while (i < N)
+{
+        if (pat[j] == txt[i])
+{
+            j++;
+            i++;
+        }
+ 
+        if (j == M)
+{
+printf("\n-----------------------");
+            printf("Found pattern at index %d ", i - j);
+            j = lps[j - 1];
+           
+        }
+ 
+        // mismatch after j matches
+        else if (i < N && pat[j] != txt[i])
+{
+       
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+}
+
+
+void computeLPSArray(char* pat, int M, int* lps)
+{
+    // counting the length of the previous longest prefix suffix
+    int len = 0;
+ 
+    lps[0] = 0;
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0) {
+                len = lps[len - 1];
+ 
+               
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+ 
+int main()
+{
+    char txt[] = "ABABDABACDABABCABAB";
+    char pat[] = "BAB";
+    KMPSearch(pat, txt);
+    return 0;
+}
+
+
+
+
+//complexity of kmp is O(n) in the worst case
+// why? when a mismatch occurs, the word itself embodies sufficient information to determine where the next match could begin
